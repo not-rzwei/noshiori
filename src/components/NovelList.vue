2d4cd1ca8992de13eda3af1e8f22ce687e4ff1b4
rif2d <saya@rifqi.id>
2020-04-15 12:44:14 +0700

    Add dummy novel list

--- src/components/NovelList.vue
+++ src/components/NovelList.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    Novel
+    <ul>
+      <li v-for="novel in list" :key="novel.title">
+        {{ novel.title }} - {{ novel.marker }} {{ novel.progress }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import Novel from "@/models/Novel";
 
 @Component
-export default class NovelList extends Vue {}
+export default class NovelList extends Vue {
+  list: Novel[] = [
+    {
+      title: "Isaac",
+      kind: Novel.Kind.WebNovel,
+      marker: Novel.Marker.Chapter,
+      progress: 31
+    },
+    {
+      title: "Tutorial is Too Hard",
+      kind: Novel.Kind.WebNovel,
+      marker: Novel.Marker.Chapter,
+      progress: 100
+    }
+  ];
+}
 </script>
